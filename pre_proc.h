a02aa85a1a1239bc004056d2330f0265e2587b4e
ethankah5-afk <ethankah5@gmail.com>
2026-04-16 12:21:58 +0300

    Update pre_proc.h

--- pre_proc.h
+++ pre_proc.h
@@ -4,7 +4,8 @@
 #ifndef ASSEMBLERPROJECT_PREPROC_H
 #define ASSEMBLERPROJECT_PREPROC_H
 #include <stdio.h>
-
+static void free_macro_table(macro_node *table, int table_size);
+char *add_new_file(char *file_name,char *ending);
 int is_macro(char *line_num,char **mcro_name);
 char* save_macro_content(FILE *fp, fpos_t* pos, int *line_count);
 int is_macro_call(char *line,char *macro_name);
