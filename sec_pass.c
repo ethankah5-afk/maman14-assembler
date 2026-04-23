33e1dbaa142f227592e238d01b01b48aca0d43a0
ethankah5-afk <ethankah5@gmail.com>
2026-04-23 11:35:01 +0300

    Update sec_pass.c

--- sec_pass.c
+++ sec_pass.c
@@ -9,37 +9,123 @@
 #include <stdlib.h>
 #include "main_struct.h"
 #include "sec_pass.h"
+//
+// Created by ethan on 23/04/2026.
+//
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include "first_pass.h"
+#include "first_pass.c"
+#include <stdlib.h>
+#include "main_struct.h"
+#include "sec_pass.h"
 
 
-Label *find_label_by_name(LableTable *labels, const char *name) {  
-
-int i; 
-    
-for (i=0; i< labels->size; i++) { 
-    if(strcmp(labels->arr[i].symbol_name, name) ==0) {
-        return &labels->arr[i]; 
-    )
-        
-return NULL; 
+Label *find_label_by_name(LableTable *labels, const char *name) {
+    int i;
+    for (i=0; i< labels->size; i++) {
+        if(strcmp(labels->arr[i].symbol_name, name) ==0) {
+            return &labels->arr[i];
+        }
+    }
+    return NULL;
 }
 
-
-int is_extern_name(NameRefTable *externs, const char *name) { 
-
+int is_extern_name(NameRefTable *externs, const char *name) {
     int i;
-
-    for ( i=0; i<(externs->size); i++) { 
-    if(strcmp(externs->arr[i].name, name) == 0) 
+    for ( i=0; i<externs->size; i++) {
+        if(strcmp(externs->arr[i].name, name) == 0){
+            return 1;
+        }
+    }
+    return 0;
+}
+int resolve_one_code_word(CodeWord *word, LableTable *labels, NameRefTable *externs) {
+    Label *lbl;
+    if(word->label == NULL) {
         return 1;
     }
-return 0; 
+    lbl = find_label_by_name(labels, word->label);
+    if (lbl ==NULL) {
+        return 0;
+    }
+    if (is_extern_name(externs, word->label)) {
+        word->value=1;
+    }
+    else {
+        word->value = (lbl->address << 2) | 2;
+    }
+    return 1;
 }
 
-
-int exe_sec_pass() {
+int resolve_code_lables(CodeImage *code_img, LableTable *labels, NameRefTable *externs) {
     int i;
-    if (error_found) {
+    for(i=0; i<code_img->size; i++) {
+        if (!resolve_one_code_word(&code_img->arr[i],labels, externs)) {
+            return 0;
+        }
+    }
+    return 1;
+}
+int mark_entry_labels(LableTable *labels, NameRefTable *entries) {
+    int i;
+    Label *lbl;
+    for(i=0; i<(entries->size);i++) {
+        lbl = find_label_by_name(labels, entries->arr[i].name);
+        if(lbl ==NULL)
+            return 0;
+        if(lbl->is_extern)
+            return 0;
+        lbl->is_entry = 1;
+    }
+    return 1;
+}
+int write_ob_file(char *file_name,CodeImage *code_img,CodeImage *data_img,int IC, int DC) {
+    FILE *fp;
+    char ob_name[100];
+    int i;
+    make_output_name(file_name,".ob",ob_name);
+
+    fp=fopen(ob_name,"w");
+    if (fp==NULL) {
         return 0;
+    }
+    fprintf(fp," %d  %d\n",IC,DC);
+    for (i=0;i<code_img->count;i++) {
+        fprintf(fp,"%s\n",convert_word_to_output(code_img->arr[i].value));
+    }
+    for (i=0;i<data_img->count;i++) {
+        fprintf(fp,"%s\n",convert_word_to_output(data_img->arr[i].value));
+    }
+    fclose(fp);
+    return 1;
+}
+int collect_entries_output(LableTable *labels, FILE *fp) {
+    int i;
+    for (i=0; i<labels->size; i++) {
+        if (labels->arr[i].is_entry) {
+            printf("ENTRY: %s %d\n", labels->arr[i].symbol_name, labels->arr[i].address);  }
+    }
+}
+int exe_sec_pass(char *file_name, LabelTable *labels, CodeImage *code_img,CodeImage *data_img,NameRefTable *externs,NameRefTable *entries,int IC,int DC) {
+    int error_found=0;
+    if (!resolve_code_labels(code_img,labels,externs)) {
+        error_found=1;
+    }
+    if (!mark_entry_labels(labels,entries)) {
+        error_found=1;
+    }
+    if (!error_found) {
+        if (!write_ob_file(file_name,code_img,data_img,IC,DC)) {
+            error_found=1;
+        }
+        if (!write_ext_file(file_name,code_img,externs)){
+            error_found=1;
+        }
+        if (!write_ent_file(file_name,labels,entries)){
+            error_found=1;
+        }
     }
     if (IC>IC_MAX) {
         print_internal_error(ERROR_CODE_54);
@@ -50,110 +136,3 @@ int exe_sec_pass() {
     }
     return 1;
 }
-
-
-int resolve_one_code_word(CodeWord *word, LableTable *labels, NameRefTable *externs) { 
-
-Label *lbl;
-if(word->label == NULL) { 
-    return 1; } 
-    
-lbl = find_label_by_name(labels, word->label);
-    
-if (lbl ==NULL) {
-     return 0; } 
-
-if (is_extern_name(externs, word->label)) { 
-    word->value=1; }
-
-else { 
-    word->value = (lbl->address << 2) | 2; }
-    return 1:
-}
-
-int resolve_code_lables(CodeImage *code_img, LableTable *labels, NameRefTable *externs) { 
-
-int i; 
-
-    for(i=0; i<(code_img->size; i++) { 
-    if (!resolve_one_code_word(&code_img->arr[i],labels, externs)) { 
-        return 0; }
-    }
-    return 1; 
-} 
-
-
-
-int mark_entry_labels(LableTable *labels, NameRefTable *entries) { 
-
-int i;
-Label lbl; 
-
-    for(i=0; i<(entries->size);i++) {
-         lbl = find_label_by_name(labels, entries->arr[i].name); 
-
-        if(lbl ==NULL) 
-            return 0; 
-        if(lbl->is_extern) 
-            return 0; 
-        lbl->is_entry = 1; 
-    } 
-    return 1; 
-        
-}
-
-
-int collect_entries_output(LableTable *labels, FILE *fp) { 
-
-    int i; 
-
-    for (i=0; i<labels->size; i++) { 
-
-        if (labels->arr[i].is_entry) { 
-
-        printf("ENTRY: %s %d\n", labels->arr[i].symbol_name, labels->arr[i].address);  } 
-    } 
-}
-
-    
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
