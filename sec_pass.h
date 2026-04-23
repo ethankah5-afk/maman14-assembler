aaef9e434a8aa3454d9972452b85136d32509f39
yakirapplebaum-cloud <yakirapplebaum@gmail.com>
2026-04-23 10:03:08 +0300

    Update sec_pass.h

--- sec_pass.h
+++ sec_pass.h
@@ -4,5 +4,7 @@
 
 #ifndef ASSEMBLERPROJECT_SEC_PASS_H
 #define ASSEMBLERPROJECT_SEC_PASS_H
+
+Label *find_label_by_name(LableTable *labels, const char *name); 
 int is_extern_name(NameRefTable *externs, const char *name); 
 #endif //ASSEMBLERPROJECT_SEC_PASS_H
