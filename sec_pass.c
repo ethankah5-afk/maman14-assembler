77ba37c260b43dab56d59ac3474c38e255da96af
yakirapplebaum-cloud <yakirapplebaum@gmail.com>
2026-04-23 10:34:08 +0300

    Update sec_pass.c

--- sec_pass.c
+++ sec_pass.c
@@ -10,41 +10,6 @@
 #include "main_struct.h"
 #include "sec_pass.h"
 
-int exe_sec_pass() {
-    int i;
-    if (error_found) {
-        return 0;
-    }
-    if (IC>IC_MAX) {
-        print_internal_error(ERROR_CODE_54);
-        return 0;
-    }
-    for(){
-
-    }
-    return 1;
-}
-
-
-int resolve_one_word(CodeWord *word, LableTable *labels, NameRefTable *externs) { 
-
-Label *lbl;
-if(word->label == NULL) { 
-    return 1; } 
-    
-lbl = find_label_by_name(labels, word->label);
-    
-if (lbl ==NULL) {
-     return 0; } 
-
-if (is_extern_name(externs, word->label)) { 
-    word->value=1; }
-
-else { 
-    word->value = (lbl->address << 2) | 2; }
-    return 1:
-}
-
 
 Label *find_label_by_name(LableTable *labels, const char *name) {  
 
@@ -71,6 +36,73 @@ return 0;
 }
 
 
+int exe_sec_pass() {
+    int i;
+    if (error_found) {
+        return 0;
+    }
+    if (IC>IC_MAX) {
+        print_internal_error(ERROR_CODE_54);
+        return 0;
+    }
+    for(){
+
+    }
+    return 1;
+}
+
+
+int resolve_one_code_word(CodeWord *word, LableTable *labels, NameRefTable *externs) { 
+
+Label *lbl;
+if(word->label == NULL) { 
+    return 1; } 
+    
+lbl = find_label_by_name(labels, word->label);
+    
+if (lbl ==NULL) {
+     return 0; } 
+
+if (is_extern_name(externs, word->label)) { 
+    word->value=1; }
+
+else { 
+    word->value = (lbl->address << 2) | 2; }
+    return 1:
+}
+
+int resolve_code_lables(CodeImage *code_img, LableTable *labels, NameRefTable *externs) { 
+
+int i; 
+
+    for(i=0; i<(code_img->size; i++) { 
+    if (!resolve_one_code_word(&code_img->arr[i],labels, externs)) { 
+        return 0; }
+    }
+    return 1; 
+} 
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
 
 
 
