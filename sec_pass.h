8c584cdcf2f5ea1f383034c08c5240c86ae2be0e
ethankah5-afk <ethankah5@gmail.com>
2026-04-23 12:26:11 +0300

    Update sec_pass.h

--- sec_pass.h
+++ sec_pass.h
@@ -1,22 +1,21 @@
 //
 // Created by ethan on 23/04/2026.
 //
+
 #ifndef ASSEMBLERPROJECT_SEC_PASS_H
 #define ASSEMBLERPROJECT_SEC_PASS_H
-
-
-#include <stdio.h>
 #include "main_struct.h"
+#include <stdio.h>
 
-Label *find_label_by_name(LableTable *labels, const char *name); 
+Label *find_label_by_name(LabelTable *labels, const char *name);
 int is_extern_name(NameRefTable *externs, const char *name);
-int resolve_one_code_word(CodeWord *word, LableTable *labels, NameRefTable *externs); 
-int resolve_code_labels(CodeImage *code_img, LableTable *labels, NameRefTable *externs);
-int mark_entry_labels(LabelTable *labels, NameRefTable *entries); 
+int resolve_one_code_word(CodeWord *word, LabelTable *labels, NameRefTable *externs);
+int resolve_code_labels(CodeImage *code_img, LabelTable *labels, NameRefTable *externs);
+int mark_entry_labels(LabelTable *labels, NameRefTable *entries);
 int exe_sec_pass(char *file_name,LabelTable *labels,CodeImage *code_img,CodeImage *data_img,NameRefTable *externs,NameRefTable *entries,int IC,int DC);
 int write_ob_file(char *file_name,CodeImage *code_img,CodeImage *data_img,int IC,int DC);
-int write_ext_file(char *file_name, CodeImage *code_img, LableTable *labels);
-int write_ent_file(char *file_name, LableTable *labels); 
+int write_ext_file(char *file_name, CodeImage *code_img, LabelTable *labels);
+int write_ent_file(char *file_name, LabelTable *labels);
 
 
 #endif //ASSEMBLERPROJECT_SEC_PASS_H
