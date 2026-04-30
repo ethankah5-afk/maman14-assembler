1c457991f4ea531dc61334a07361554be00d207d
ethankah5-afk <ethankah5@gmail.com>
2026-04-30 15:52:33 +0300

    Update sec_pass.h

--- sec_pass.h
+++ sec_pass.h
@@ -4,9 +4,12 @@
 
 #ifndef ASSEMBLERPROJECT_SEC_PASS_H
 #define ASSEMBLERPROJECT_SEC_PASS_H
+#include <string.h>
 #include "main_struct.h"
 #include <stdio.h>
 
+void make_output_name(char *file_name,char *ext, char *out);
+char *convert_word_to_output(unsigned short word);
 Label *find_label_by_name(LabelTable *labels, const char *name);
 int is_extern_name(NameRefTable *externs, const char *name);
 int resolve_one_code_word(CodeWord *word,int index, LabelTable *labels, NameRefTable *externs);
@@ -14,7 +17,7 @@ int resolve_code_labels(CodeImage *code_img, LabelTable *labels, NameRefTable *e
 int mark_entry_labels(LabelTable *labels, NameRefTable *entries);
 int exe_sec_pass(char *file_name,LabelTable *labels,CodeImage *code_img,CodeImage *data_img,NameRefTable *externs,NameRefTable *entries,int IC,int DC);
 int write_ob_file(char *file_name,CodeImage *code_img,CodeImage *data_img,int IC,int DC);
-int write_ext_file(char *file_name, CodeImage *code_img, LabelTable *labels);
+int write_ext_file(char *file_name, CodeImage *code_img,NameRefTable *externs);
 int write_ent_file(char *file_name, LabelTable *labels);
 
 
