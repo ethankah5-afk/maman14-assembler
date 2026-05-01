6ea419170ff6892ffc6fba9122a680a1e8333529
ethankah5-afk <ethankah5@gmail.com>
2026-05-01 12:39:10 +0300

    Update pre_proc.h

--- pre_proc.h
+++ pre_proc.h
@@ -10,5 +10,4 @@ int is_macro(char *line_num,char **mcro_name);
 char* save_macro_content(FILE *fp, fpos_t* pos, int *line_count);
 int is_macro_call(char *line,char *macro_name);
 int run_preproc(char *file_name);
-
 #endif //ASSEMBLERPROJECT_PREPROC_H
