d2ec8ec830209f75f81ab8cdce4bb528ed6464c1
ethankah5-afk <ethankah5@gmail.com>
2026-05-01 12:46:22 +0300

    Update conversion.h

--- conversion.h
+++ conversion.h
@@ -4,4 +4,8 @@
 
 #ifndef ASSEMBLERPROJECT_CONVERSION_H
 #define ASSEMBLERPROJECT_CONVERSION_H
+unsigned short regToShort(char *reg_before);
+void toBinary12(unsigned short value, char *binary);
+char *find_first_data_number(char *line);
+
 #endif //ASSEMBLERPROJECT_CONVERSION_H
