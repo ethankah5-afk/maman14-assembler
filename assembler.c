486f2aec1e06b20de29478d6fbd62711ae2942c3
ethankah5-afk <ethankah5@gmail.com>
2026-05-01 12:41:09 +0300

    Update assembler.c

--- assembler.c
+++ assembler.c
@@ -9,41 +9,24 @@
 #include "main_struct.h"
 #include "pre_proc.h"
 int main(int argc, char *argv[]) {
-    
-    node *macro_table; 
-    macro_table = NULL; 
-    
     int i;
-
     for (i = 1; i < argc; i++) {
-
         char *as_file;
         char *am_file;
-
         /* שלב 1: preproc */
         as_file = add_new_file(argv[i], ".as");
-
-        if (!run_preproc(as_file,&macro_table)) {
+        if (!run_preproc(as_file)) {
             printf("Preproc failed\n");
             free(as_file);
-            free_macro_list(macro_table);
             continue;
         }
-        /* שלב 2: first pass */
+        /* שב 2: first pass */
         am_file = add_new_file(argv[i], ".am");
-
-        if (!exe_first_pass(am_file),macro_table) {
-        free(as_file);
-        free(am_file);
-        free_macro_list(macro_table);
-        printf("First pass failed\n");
-            continue;
+        if (!exe_first_pass(am_file)) {
+            printf("First pass failed\n");
         }
-
         free(as_file);
         free(am_file);
-        free_macro_list(macro_table);
     }
-
     return 0;
 }
