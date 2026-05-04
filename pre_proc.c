88aeef7c25d659c13c093d18c9be12d8b67e29a2
ethankah5-afk <ethankah5@gmail.com>
2026-05-04 12:10:19 +0300

    Update pre_proc.c

--- pre_proc.c
+++ pre_proc.c
@@ -6,6 +6,8 @@
 #include <stdlib.h>
 #include "main_struct.h"
 #include "pre_proc.h"
+#include "errors.h"
+#include "first_pass.h"
 #define MAX_LINE_LENGTH 81
 
 void free_macro_table(macro_node *table, int table_size) {
@@ -37,7 +39,7 @@ char *add_new_file(char *file_name, char *ending) {
     return new_file_name;
 }
 
-int is_macro(char *line_num,char **mcro_name) {
+int is_macro(char *line_num,char **mcro_name,ERROR_CODES *error_code) {
     char *token;
     char *extra;
     token= strtok(line_num," \t\n");
@@ -46,28 +48,60 @@ int is_macro(char *line_num,char **mcro_name) {
         return 0;
     }
     token= strtok(NULL," \t\n");
-    if (token == NULL) return -1;
+    if (token == NULL) {
+        *error_code=ERROR_9;
+        return -1;
+    }
     *mcro_name=token;
     extra = strtok(NULL," \t\n");
-    if (extra != NULL) return -1;
+    if (extra != NULL) {
+        *error_code=ERROR_10;
+        return -1;
+    }
     return 1;
 }
 
-char* save_macro_content(FILE *fp, fpos_t* pos, int *line_count) {
+char* save_macro_content(FILE *fp, fpos_t* pos, int *line_count,ERROR_CODES *error_code) {
+    char *p;
+    int found_end=0;
     int mcro_length=0;
     char *mcro;
     char str[MAX_LINE_LENGTH];
+    char str_copy[MAX_LINE_LENGTH];
     if (fsetpos(fp,pos)!=0) {
+        *error_code=ERROR_11;
         return NULL;
     }
     str[0]='\0';
-    while (fgets(str,MAX_LINE_LENGTH,fp)!=NULL &&strncmp(str,"mcroend",7)!=0) {
-        mcro_length+=(int)strlen(str);
+    while (fgets(str,MAX_LINE_LENGTH,fp)!=NULL) {
         (*line_count)++;
+        strcpy(str_copy,str);
+        trim_spaces(str_copy);
+        if (strncmp(str_copy,"mcroend",7)==0) {
+            p=str_copy+7;
+            while (*p==' '||*p=='\t') {
+                p++;
+            }
+            if (*p!='\n'&&*p!='\0') {
+                *error_code=ERROR_12;
+                return NULL;
+            }
+            found_end=1;
+            break;
+        }
+        mcro_length+=(int)strlen(str);
+    }
+    if (!found_end) {
+        *error_code=ERROR_15;
+        return NULL;
     }
     mcro = (char *)malloc(mcro_length+1);
-    if (mcro==NULL)return NULL;
+    if (mcro==NULL) {
+        *error_code=ERROR_1;
+        return NULL;
+    }
     if (fsetpos(fp,pos)!=0) {
+        *error_code=ERROR_11;
         free(mcro);
         return NULL;
     }
@@ -120,54 +154,67 @@ int run_preproc(char *file_name,macro_node **macro_table,int *macro_count) {
     int macro_status;
     int found;
     int i;
-    /* פתיחת קבצים */
+    int error_found=0;
+    ERROR_CODES error_code;
+    location file_location;
     fp_in = fopen(file_name, "r");
-    if (fp_in == NULL) return 0;
-
+    if (fp_in == NULL) {
+        print_internal_error(ERROR_8);
+        return 0;
+    }
     am_file = add_new_file(file_name, ".am");
     if (am_file==NULL) {
+        print_internal_error(ERROR_1);
         fclose(fp_in);
         return 0;
     }
     fp_out = fopen(am_file, "w");
     if (fp_out == NULL) {
+        print_internal_error(ERROR_7);
         fclose(fp_in);
         free(am_file);
         return 0;
     }
-
     while (fgets(line,MAX_LINE_LENGTH , fp_in)) {
         line_count++;
+        file_location.file_name = file_name;
+        file_location.line_num = line_count;
         strcpy(line_copy, line);
-        macro_status=is_macro(line_copy,&mcro_name);
+        macro_status=is_macro(line_copy,&mcro_name,&error_code);
         if (macro_status==-1) {
-            fclose(fp_in);
-            fclose(fp_out);
-            free_macro_table(table,table_size);
-            free(am_file);
-            return 0;
+            print_external_error(error_code,file_location);
+            error_found=1;
+            continue;
         }
         if (macro_status==1) {
             /* שמור מיקום תחילת תוכן המאקרו */
             if (fgetpos(fp_in, &pos)!=0) {
+                print_internal_error(ERROR_11);
                 fclose(fp_in);
                 fclose(fp_out);
                 free_macro_table(table,table_size);
                 free(am_file);
                 return 0;
             }
-            content = save_macro_content(fp_in, &pos, &line_count);
+            content = save_macro_content(fp_in, &pos, &line_count,&error_code);
             if (content == NULL) {
-                fclose(fp_in);
-                fclose(fp_out);
-                free_macro_table(table,table_size);
-                free(am_file);
-                return 0;
+                if (error_code==ERROR_1||error_code==ERROR_11) {
+                    print_internal_error(error_code);
+                    fclose(fp_in);
+                    fclose(fp_out);
+                    free_macro_table(table,table_size);
+                    free(am_file);
+                    return 0;
+                }
+                print_external_error(error_code, file_location);
+                error_found = 1;
+                continue;
             }
 
             /* הוסף לטבלה */
             temp = (macro_node *)realloc(table, (table_size + 1) * sizeof(macro_node));
             if (temp==NULL) {
+                print_internal_error(ERROR_1);
                 fclose(fp_in);
                 fclose(fp_out);
                 free(content);
@@ -180,6 +227,7 @@ int run_preproc(char *file_name,macro_node **macro_table,int *macro_count) {
             table[table_size].content = NULL;
             table[table_size].name = (char *)malloc(strlen(mcro_name) + 1);
             if (table[table_size].name==NULL) {
+                print_internal_error(ERROR_1);
                 fclose(fp_in);
                 fclose(fp_out);
                 free(content);
@@ -209,12 +257,16 @@ int run_preproc(char *file_name,macro_node **macro_table,int *macro_count) {
         }
 
     }
-
-    /* ניקוי */
-    *macro_table=table;
-    *macro_count=table_size;
     fclose(fp_in);
     fclose(fp_out);
     free(am_file);
+    if (error_found) {
+        free_macro_table(table,table_size);
+        *macro_table=NULL;
+        *macro_count=0;
+        return 0;
+    }
+    *macro_table=table;
+    *macro_count=table_size;
     return 1;
 }
