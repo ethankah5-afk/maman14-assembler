b9562ef1d13484eced89fbb38ebe818f2b3f8536
ethankah5-afk <ethankah5@gmail.com>
2026-05-08 08:23:02 +0300

    Update errors.c

--- errors.c
+++ errors.c
@@ -4,29 +4,31 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdarg.h>
-#include "Errors.h"
+#include "errors.h"
+#include "main_struct.h"
 
 /* The Following array specifies the error code numbers and the corresponding error message.
  * Missing lines are intentionally left blank for future error codes
  * */
 Error errors[] = {
         {ERROR_0,  "No Error"},
-        {ERROR_1,  "Failed to dynamically allocate memory"},
-        {ERROR_2,  ".as files name is missing"},
-        {ERROR_3,  "The file name is too long"},
-        {ERROR_4,  "The file name does not exist"},
-        {ERROR_5,  ".am file is missing"},
-        {ERROR_6,  "The Line is too long"},
-        {ERROR_7,  "Failed to open new file for writing"},
-        {ERROR_8,  "Failed to open file for reading"},
-        {ERROR_9,  "Error - macro without name definition"},
-        {ERROR_10, "Error - extra text after macro name definition"},
-        {ERROR_11, "Setting a position in a file failed"},
-        {ERROR_12, "endmcro with extra text"},
-        {ERROR_13, "Macro has more than one definition"},
+        {ERROR_1,  "Failed to allocate memory dynamically (also refers to creating files)"},
+        {ERROR_2,  ".as file name is missing"},
+        {ERROR_3,  "file name is too long"},
+        {ERROR_4,  "file does not exist"},
+        {ERROR_5,  ".am file is missing"},//
+        {ERROR_6,  "Line is too long"},
+        {ERROR_7,  "Failed to open output file for writing"},
+        {ERROR_8,  "Failed to open input file for reading"},
+        {ERROR_9,  "Macro definition is missing a macro name"},
+        {ERROR_10, "Extra text after macro name definition"},
+        {ERROR_11, "Failed to set or get position in file"},
+        {ERROR_12, "Extra text after mcroend"},
+        {ERROR_13, "Macro is defined more than once"},//
         {ERROR_14, "Failed to copy file during macros expansion"},
         {ERROR_15, "Macros expansion in an .as file failed"},
         {ERROR_16, "Macro has an illegal name"},
+        {-1,            ""},
         {-1,            ""},
         {-1,            ""},
         {-1,            ""},
@@ -45,7 +47,7 @@ Error errors[] = {
         {ERROR_33, "Illegal argument"},
         {ERROR_34, "Missing argument"},
         {ERROR_35, "Missing comma between argument in a command line with two arguments"},
-        {ERROR_36, "The label is used in the code, but it was never defined"},
+        {ERROR_36, "Label not defined in the assembly file"},
         {ERROR_37, "Illegal label after .entry"},
         {ERROR_38, "Illegal comma near opcode"},
         {ERROR_39, "More commas than needed"},
@@ -58,29 +60,39 @@ Error errors[] = {
         {ERROR_46, "Illegal register name. Use only @r1-@r7"},
         {ERROR_47, "Illegal comma before opcode"},
         {ERROR_48, ""},
-        {ERROR_49, "Data line without '. before directive"},
+        {ERROR_49, "Data line without '.' before directive"},
         {ERROR_50, "Instruction '.data' line contains non-number info"},
         {ERROR_51, "Comma after the last number in a '.data' line"},
         {ERROR_52, "Missing '\"' after '.string'"},
         {ERROR_53, "Extra text after the string end in '.string' line"},
         {ERROR_54, "IC too big for word CPU word length"},
         {ERROR_55, "Label definition is written more than once"},
-        {ERROR_56, "Label defined as .extern and .entry"},
+        {ERROR_56, "Label defined as .extern and defined in file"},
         {ERROR_57, "Input number in .data line is out of range"},
         {ERROR_58, "Illegal data line directive"},
         {ERROR_59, "Instruction '.data' line contains illegal chars or syntax error"}
 
 };
 
-void print_internal_error(int ERROR) {
-    /* Print the error code number and the error message */
-    printf("~~ERROR: ID:%d~~ | %s\n", ERROR, errors[ERROR].error_msg);
+char *get_error_msg(int error_code){
+    int i;
+    int errors_count;
+    errors_count = (int)(sizeof(errors) / sizeof(errors[0]));
+    for (i = 0; i < errors_count; i++) {
+        if (errors[i].error_id == error_code) {
+            return errors[i].error_msg;
+        }
+    }
+    return "Unknown error";
 }
 
-void print_external_error(int ERROR, location file) {
-    /* Print the error code number, file name, assembly line number and the error message */
-    printf("~~ERROR: ID:%d~~ in %s at line:%d | there is error: %s\n", ERROR, \
-    file.file_name, file.line_num, errors[ERROR].error_msg);
+void print_internal_error(int error_code)
+{
+    printf("ERROR!: ID:%d  %s\n", error_code, get_error_msg(error_code));
 }
 
+void print_external_error(int error_code, location file)
+{
+    printf("ERROR!: ID:%d in %s at line:%d  the error is: %s\n",error_code,file.file_name,file.line_num,get_error_msg(error_code));
+}
 //
