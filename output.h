d0b6ea771a16cecd3d85bd0dff89795057b356f7
ethankah5-afk <ethankah5@gmail.com>
2026-05-10 19:42:24 +0300

    Update output.h

--- output.h
+++ output.h
@@ -9,6 +9,6 @@
 void make_output_name(char *file_name, char *ext, char *out);
 int write_ent_file(char *file_name,LabelTable *labels);
 int write_ext_file(char *file_name, CodeImage *code_img,NameRefTable *externs);
-int write_ob_file(char *file_name,CodeImage *code_img,CodeImage *data_img,int IC, int DC);
+int write_ob_file(char *file_name,CodeImage *code_img,CodeImage *data_img,NameRefTable *externs,int IC, int DC);
 
 #endif //ASSEMBLERPROJECT_OUTPUT_H
