be690456a069ab03fa431d067793f1f819fe7951
ethankah5-afk <ethankah5@gmail.com>
2026-05-10 19:40:47 +0300

    Update output.c

--- output.c
+++ output.c
@@ -8,6 +8,7 @@
 #include "sec_pass.h"
 #include "output.h"
 #include "constants.h"
+#include "encoder.h"
 
 void make_output_name(char *file_name, char *ext, char *out) {
     char *dot;
@@ -37,7 +38,7 @@ int write_ent_file(char *file_name,LabelTable *labels) {
     }
     for (i=0; i<labels->count; i++) {
         if (labels->arr[i].is_entry) {
-            fprintf(fp,"%s %d\n",labels->arr[i].symbol_name,labels->arr[i].address);
+            fprintf(fp,"%s %04d\n",labels->arr[i].symbol_name,labels->arr[i].address);
         }
     }
     fclose(fp);
@@ -57,7 +58,7 @@ int write_ext_file(char *file_name, CodeImage *code_img,NameRefTable *externs) {
     found=0;
     for (i=0; i<code_img->count;i++) {
         if (code_img->arr[i].label!=NULL&&is_extern_name(externs,code_img->arr[i].label)) {
-            fprintf(fp, "%s %d\n",code_img->arr[i].label,100+i);
+            fprintf(fp, "%s %04d\n",code_img->arr[i].label,100+i);
             found=1;
         }
     }
@@ -67,25 +68,38 @@ int write_ext_file(char *file_name, CodeImage *code_img,NameRefTable *externs) {
     }
     return 1;
 }
-int write_ob_file(char *file_name,CodeImage *code_img,CodeImage *data_img,int IC, int DC) {
+int write_ob_file(char *file_name,CodeImage *code_img,CodeImage *data_img,NameRefTable *externs,int IC, int DC) {
     FILE *fp;
     char ob_name[100];
     int i;
     char out_word[4];
+    unsigned short value_to_print;
+    char are;
     make_output_name(file_name,".ob",ob_name);
     fp=fopen(ob_name,"w");
     if (fp==NULL) {
         print_internal_error(ERROR_7);
         return 0;
     }
-    fprintf(fp,"%d %d \n", IC -IC_INIT_VALUE, DC);
+    fprintf(fp,"%5d %d \n", IC -IC_INIT_VALUE, DC);
     for(i=0; i< code_img->count; i++) {
-        convert_word_to_hex(code_img->arr[i].value,out_word);
-        fprintf(fp, "%04d %s\n",IC_INIT_VALUE+i, out_word);
+        value_to_print=code_img->arr[i].value;
+        are='A';
+        if (code_img->arr[i].label!=NULL){
+            if ((value_to_print & 3)==ARE_EXTERNAL){
+                are='E';
+            }
+            else if ((value_to_print & 3)==ARE_RELOCATABLE){
+                are='R';
+            }
+            value_to_print=(unsigned short)(value_to_print>>2);
+        }
+        convert_word_to_hex(value_to_print,out_word);
+        fprintf(fp, "%04d %s %c\n",IC_INIT_VALUE+i, out_word,get_are_char(&code_img->arr[i],externs));
     }
     for(i=0; i<data_img->count; i++) {
         convert_word_to_hex(data_img->arr[i].value,out_word);
-        fprintf(fp, "%04d %s\n",IC_INIT_VALUE+code_img->count+i, out_word);
+        fprintf(fp, "%04d %s A\n",IC_INIT_VALUE+code_img->count+i, out_word);
     }
     fclose(fp);
     return 1;
