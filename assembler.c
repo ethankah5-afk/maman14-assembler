205ff2a803dd80472be3b54c071e354f268b0131
yakirapplebaum-cloud <yakirapplebaum@gmail.com>
2026-05-10 20:28:34 +0300

    Update assembler.c

--- assembler.c
+++ assembler.c
@@ -1,35 +1,40 @@
-//
-// Created by ethan on 25/03/2026.
-//
+/*
+* Created by Ethan and Yakir
+*/
 #include <stdio.h>
 #include <ctype.h>
 #include "first_pass.h"
 #include <stdlib.h>
 #include "main_struct.h"
 #include "pre_proc.h"
+
 int main(int argc, char *argv[]) {
     macro_node *macro_table=NULL;
     int macro_count=0;
     int i;
-
+/* check that at least one file was received */
     if (argc < 2) {
         print_internal_error(ERROR_2);
         return 0;
     }
-
+/*Go over all the files received from the command line */
     for (i = 1; i < argc; i++) {
 
         char *as_file;
         char *am_file;
-
+        
+    /*Create as. file name */
         as_file = add_new_file(argv[i], ".as");
 
         if(as_file ==NULL) {
             print_internal_error(ERROR_1);
             return 0;
         }
-
+        
+/* Run preprocessor stage: expands macros and creates .am file  */
         if (!run_preproc(as_file,&macro_table,&macro_count)) {
+            
+            /* Free macro table if preprocessing failed */
             if (macro_table != NULL) {
                 free_macro_table(macro_table, macro_count);
                 macro_table = NULL;
@@ -37,6 +42,7 @@ int main(int argc, char *argv[]) {
             free(as_file);
             continue;
         }
+        /* Create .am file name */
         am_file = add_new_file(argv[i], ".am");
         if(am_file == NULL) {
             print_internal_error(ERROR_1);
@@ -46,7 +52,9 @@ int main(int argc, char *argv[]) {
             free(as_file);
             return 0;
         }
+        /* Run assembler passes on .am file */
         exe_passes(am_file,macro_table,macro_count);
+        /* Free macro table after finishing current file */
         free_macro_table(macro_table,macro_count);
         macro_table=NULL;
         macro_count=0;
