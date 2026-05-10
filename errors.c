cbc5f22cd69717929bb8404747b3ed73f268653a
ethankah5-afk <ethankah5@gmail.com>
2026-05-10 19:36:51 +0300

    Update errors.c,

--- errors.c
+++ errors.c
@@ -1,11 +1,9 @@
 //
 // Created by ethan on 04/05/2026.
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <stdarg.h>
 #include "errors.h"
 #include "main_struct.h"
+
 
 /* The Following array specifies the error code numbers and the corresponding error message.
  * Missing lines are intentionally left blank for future error codes
@@ -16,39 +14,41 @@ Error errors[] = {
         {ERROR_2,  ".as file name is missing"},
         {ERROR_3,  "file name is too long"},
         {ERROR_4,  "file does not exist"},
-        {ERROR_5,  ".am file is missing"},
+        {ERROR_5,  ".am file is missing"},//
         {ERROR_6,  "Line is too long"},
         {ERROR_7,  "Failed to open output file for writing"},
         {ERROR_8,  "Failed to open input file for reading"},
         {ERROR_9,  "Macro definition is missing a macro name"},
-        {ERROR_10, "Extra text after macro definition"},
+        {ERROR_10, "Extra text after macro name definition"},
         {ERROR_11, "Failed to set or get position in file"},
         {ERROR_12, "Extra text after mcroend"},
+        {ERROR_13, "Macro is defined more than once"},//
         {ERROR_14, "Failed to copy file during macros expansion"},
         {ERROR_15, "Macros expansion in an .as file failed"},
-        {ERROR_16, "Illegal macro name"},
+        {ERROR_16, "Macro has an illegal name"},
         {ERROR_17, "not valid opcode"},
-        {ERROR_32, "Line contains extra text"},//18
-        {ERROR_33, "Illegal argument"},//19
-        {ERROR_34, "Missing argument"},//20
-        {ERROR_35, "Missing comma between operands"},//21
-        {ERROR_36, "Undefined label"},//22
-        {ERROR_37, "Illegal label after .entry"},//23
-        {ERROR_39, "More commas than needed"},//24
-        {ERROR_40, "Comma in the wrong place"},//25
-        {ERROR_42, "Illegal char"},//26
-        {ERROR_43, "Missing comma between numbers"},//27
-        {ERROR_44, "Illegal label"},//28
-        {ERROR_47, "Illegal comma before opcode"},//29
-        {ERROR_49, "Data line without '.' before directive"},//30
-        {ERROR_50, "Instruction '.data' line contains non-number info"},//31
-        {ERROR_51, "Comma after the last number in a '.data' line"},//32
-        {ERROR_52, "Missing quotes in .string"},//33
-        {ERROR_53, "Extra text after .string"},//34
-        {ERROR_55, "Label definition is written more than once"},//35
-        {ERROR_56, "Label defined as .extern and .entry"},//36
-        {ERROR_57, "Input number in .data line is out of range"},//37
-        {ERROR_58, "Illegal data line directive"},//38
+        {ERROR_18, "Line contains extra text"},
+        {ERROR_19, "Illegal argument"},
+        {ERROR_20, "Missing argument"},
+        {ERROR_21, "Missing comma between argument in a command line with two arguments"},
+        {ERROR_22, "Label not defined in the assembly file"},
+        {ERROR_23, "Illegal label after .entry"},
+        {ERROR_24, "More commas than needed"},
+        {ERROR_25, "Comma in the wrong place"},
+        {ERROR_26, "Illegal char"},
+        {ERROR_27, "Missing comma between numbers"},
+        {ERROR_28, "Illegal label declaration"},
+        {ERROR_29, "Illegal comma before opcode"},
+        {ERROR_30, "Data line without '.' before directive"},
+        {ERROR_31, "Instruction '.data' line contains non-number info"},
+        {ERROR_32, "Comma after the last number in a '.data' line"},
+        {ERROR_33, "Missing '\"' after '.string'"},
+        {ERROR_34, "Extra text after the string end in '.string' line"},
+        {ERROR_35, "Label definition is written more than once"},
+        {ERROR_36, "Label defined as .extern and defined in file"},
+        {ERROR_37, "Input number in .data line is out of range"},
+        {ERROR_38, "Illegal data line directive"},
+
 };
 
 char *get_error_msg(int error_code){
