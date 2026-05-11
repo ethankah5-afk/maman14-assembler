b676fbb762d67cd644ed5dc66c045c950e0f7a86
yakirapplebaum-cloud <yakirapplebaum@gmail.com>
2026-05-11 09:49:56 +0300

    Update encoder.h

--- encoder.h
+++ encoder.h
@@ -1,13 +1,20 @@
-//
-// Created by ethan on 08/05/2026.
-//
+  /* 
+  * Created by Ethan and Yakir 
+  */
 
 #ifndef ASSEMBLERPROJECT_ENCODER_H
 #define ASSEMBLERPROJECT_ENCODER_H
 
 #include "main_struct.h"
 
+/* 
+  * Get ARE for code word 
+  * word - code word 
+  * externs - extern labels table 
+  *return - ARE character 
+  */
 char get_are_char(CodeWord *word, NameRefTable *externs);
+
 unsigned short build_first_word(Instruction *inst, char *op1, char *op2, int op_count);
 unsigned short encode_immediate(char *op);
 int handle_instruction_line(char *line,int line_num,char *file_name,LabelTable *labels,CodeImage *code_img,int *IC,macro_node *macro_table,int macro_count);
@@ -15,5 +22,11 @@ int handle_one_operand(char *op,int line_num,char *file_name,LabelTable *labels,
 int is_legal_addressing(Instruction *inst, int src_type, int dest_type, int op_count);
 int is_one_of(int type, int a, int b, int c, int d);
 unsigned short encode_register(char *op);
+
+/*
+* Get operand addressing type 
+* op - operand string 
+* return - operand addressing type 
+*/
 int get_addressing_type(char *op);
-#endif //ASSEMBLERPROJECT_ENCODER_H
+#endif    /* ASSEMBLERPROJECT_ENCODER_H */
