d22f98aa36b0dfc57712ed193df1ec8aedf9c905
yakirapplebaum-cloud <yakirapplebaum@gmail.com>
2026-05-11 16:32:46 +0300

    Update tables.c

--- tables.c
+++ tables.c
@@ -1,10 +1,17 @@
-//
-// Created by ethan on 08/05/2026.
-//
+/* 
+* Created by Ethan and Yakir
+*/
+
 #include <stdlib.h>
 #include <string.h>
 #include "main_struct.h"
 #include "tables.h"
+
+/*
+* Initialize labels table 
+* table - labels table 
+* return - 1 if success otherwise 0 
+*/
 int initLabelTable(LabelTable *table) {
     table->count = 0;
     table->capacity = 10;
@@ -14,12 +21,19 @@ int initLabelTable(LabelTable *table) {
     }
     return 1;
 }
+/*
+* Find label by name 
+* table - labels table
+* name - label name
+* return - label index if found otherwise -1 
+*/
 
 int findLabel(LabelTable *table,char *name) {
     int i;
     if (table==NULL||name==NULL) {
         return -1;
     }
+    /* Search labels table */
     for (i = 0; i < table->count; i++) {
         if (strcmp(table->arr[i].symbol_name, name) == 0) {
             return i;
@@ -28,11 +42,24 @@ int findLabel(LabelTable *table,char *name) {
     return -1;
 }
 
+/*
+* Add new label to labels table 
+* table - labels table 
+* name - label name 
+* address - label address 
+* is_data - data label flag 
+* line_defined - definition line number 
+* return - 1 if success otherwise 0 
+*/
+
 int addLabel(LabelTable *table, char *name, int address, int is_data, int line_defined) {
     Label *temp;
+    
+    /* Prevent duplicate labels */
     if (findLabel(table, name) != -1) {
         return 0;
     }
+    /* Increase table size if is needed  */
     if (table->count == table->capacity) {
         table->capacity *= 2;
         temp = (Label *)realloc(table->arr, table->capacity * sizeof(Label));
@@ -40,10 +67,12 @@ int addLabel(LabelTable *table, char *name, int address, int is_data, int line_d
             return 0;
         }
         table->arr = temp;
-    }
-    if (strlen(name)>30) {
+    } 
+
+    /* label length limit */
+    if (strlen(name)>31) {
         return 0;
-    }
+    } 
     strcpy(table->arr[table->count].symbol_name, name);
     table->arr[table->count].address = address;
     table->arr[table->count].line_defined = line_defined;
@@ -53,6 +82,11 @@ int addLabel(LabelTable *table, char *name, int address, int is_data, int line_d
     table->count++;
     return 1;
 }
+
+/* 
+* Free labels table memory
+* table - labels table
+*/
 void free_label_table(LabelTable *table) {
     if (table->arr != NULL) {
         free(table->arr);
@@ -63,7 +97,11 @@ void free_label_table(LabelTable *table) {
     table->capacity = 0;
 }
 
-
+/* 
+* Initialize code image 
+* img - code image 
+* return - 1 if success otherwise 0 
+*/
 int init_code_image(CodeImage *img) {
     img->count = 0;
     img->capacity = 10;
@@ -73,8 +111,19 @@ int init_code_image(CodeImage *img) {
     }
     return 1;
 }
+
+/* 
+* Add code word to to code image
+* img - code image 
+* value - machine word value 
+* label - unresolved label 
+* line - assembly line number 
+* return - 1 if success otherwise 0 
+*/
 int add_code_word(CodeImage *img, unsigned short value, const char *label, int line) {
     CodeWord *temp;
+
+    /* add to code image size if needed */
     if (img->count == img->capacity) {
         img->capacity *= 2;
         temp = (CodeWord *)realloc(img->arr, img->capacity * sizeof(CodeWord));
@@ -100,9 +149,15 @@ int add_code_word(CodeImage *img, unsigned short value, const char *label, int l
     img->count++;
     return 1;
 }
+
+/*
+* Free code image memory 
+* img - code image
+*/
 void free_code_image(CodeImage *img) {
     int i;
-
+    
+    /* free all label strings */
     for (i = 0; i < img->count; i++) {
         if (img->arr[i].label != NULL) {
             free(img->arr[i].label);
@@ -115,7 +170,11 @@ void free_code_image(CodeImage *img) {
     img->capacity = 0;
 }
 
-
+/* 
+* Initialize name references table
+* table -  name references table
+* return - 1 success otherwise 0 
+*/
 int  init_name_ref_table(NameRefTable *table) {
     table->count = 0;
     table->capacity = 10;
@@ -125,9 +184,17 @@ int  init_name_ref_table(NameRefTable *table) {
     }
     return 1;
 }
+/*  
+* Add name to reference table
+* table - name references table
+* name - referenced name 
+*line - assembly line number
+* return -  1 success otherwise 0 
+ */
 int add_name_ref(NameRefTable *table, const char *name, int line) {
     NameRef *temp;
 
+     */ increase table size if is needed */
     if (table->count == table->capacity) {
         table->capacity *= 2;
         temp = (NameRef *)realloc(table->arr, table->capacity * sizeof(NameRef));
@@ -145,8 +212,15 @@ int add_name_ref(NameRefTable *table, const char *name, int line) {
     table->count++;
     return 1;
 }
+
+/*
+* Free name references table memory 
+* table - name references table
+*/
 void free_name_ref_table(NameRefTable *table) {
     int i;
+
+    /* Free all referenced names */
     for (i = 0; i < table->count; i++) {
         free(table->arr[i].name);
     }
