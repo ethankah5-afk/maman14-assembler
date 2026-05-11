f3e23f1845fd959854dc254d88c66364b0d03ff5
yakirapplebaum-cloud <yakirapplebaum@gmail.com>
2026-05-11 09:35:17 +0300

    Update conversion.h

--- conversion.h
+++ conversion.h
@@ -3,16 +3,34 @@
 #ifndef ASSEMBLERPROJECT_CONVERSION_H
 #define ASSEMBLERPROJECT_CONVERSION_H
 
-/* Convert Register name into binary representation */
+/* 
+* Convert register name to its binary representaion
+* reg_before - register name 
+* return - register binary value 
+*/
 unsigned short regToShort(char *reg_before);
 
-/* Convert 12 bit value into binary string */
+/* 
+* Convert a 12 bit value into a binary string
+* value - number to convert 
+* binary - output
+*/
+
 void toBinary12(unsigned short value, char *binary);
 
-/* Find first number afterr .data */
+/* 
+* find the first number after .data
+* line - source line 
+* return - pointer to the frist data number 
+*/
 char *find_first_data_number(char *line);
 
-/* Convert machine word into hexadecimal string */
+/* 
+* Convert a machine word into a hexadecimal string
+* word - machine to to convert 
+* res - output hexadecimal string 
+* return - pointer to result 
+*/
 char *convert_word_to_hex(unsigned short word,char res[4]);
 
 #endif  */  ASSEMBLERPROJECT_CONVERSION_H */
